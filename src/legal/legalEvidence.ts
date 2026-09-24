14f47aa4cfdfb1b547e9fb1659603b3bd03a0f08
Georlan <129700145+Georlan@users.noreply.github.com>
2026-09-24 15:00:53 -0300

    legal: pin v3.2 provenance

--- src/legal/legalEvidence.ts
+++ src/legal/legalEvidence.ts
@@ -1,7 +1,7 @@
-import { LEGAL_DOCUMENTS, LEGAL_VERSION, type LegalDocument } from './legalContentV31';
+import { LEGAL_DOCUMENTS, LEGAL_VERSION, type LegalDocument } from './legalContentV32';
 
-export const LEGAL_SOURCE_COMMIT = 'c38ac9e62aff753d141b5bde05db3eabbfdffde8';
-export const LEGAL_SOURCE_BLOB_SHA = '63cbddd9cc91b08fa1190a4460d62d61d3ea73e6';
+export const LEGAL_SOURCE_COMMIT = '70015843a88c09eff0edf503897de2a0abe33fcf';
+export const LEGAL_SOURCE_BLOB_SHA = '4783f58da5481a59badf868b07eb42c80c0b5b3a';
 
 const bySlug = new Map(LEGAL_DOCUMENTS.map(document => [document.slug, document]));
 
